4bf996fa1412613454a2bcf3ad8ea9e9719bf021
Harshit Ranjan <harshitranjan0000@gmail.com>
2025-01-24 10:36:37 +0530

    fixed_2

--- src/app/catalouge/page.tsx
+++ src/app/catalouge/page.tsx
@@ -47,11 +47,11 @@ const data = [
 const page = () => {
   return (
     <div className={styles.background}>
-      <div className="w-full h-[15vh]  flex justify-center items-center">
+      <div className="w-full h-[22vh]  flex justify-center items-center bg-transparent">
         <Link href={"/"} className="text-white
         absolute left-[3%] w-[3vw] h-[3vw] bg-slate-500/65 flex justify-center items-center text-3xl rounded-full 
         "><GoArrowLeft /></Link>
-        <h1 className="lg:text-[2.8vw] pt-[3vh] lg:pt-0  text-[10vw] font-semibold text-center text-white">
+        <h1 className="lg:text-[2.8vw] pt-[10vh] lg:pt-0  text-[10vw] font-semibold text-center text-white">
           CATALOGUE
         </h1>
       </div>
